a8bdc81a1bd85e74100ce1b695d39af8111b40d6
mvu2ab decina <mvu2ab@virginia.edu>
2024-05-04 02:41:18 +0000

    DP 3

--- get-message.py
+++ get-message.py
@@ -1,10 +1,11 @@
+
 import boto3
 from botocore.exceptions import ClientError
 import requests
 import json
 
 # Set up your SQS queue URL and boto3 client
-url = "https://sqs.us-east-1.amazonaws.com/440848399208/xxxxxxx"
+url = "https://sqs.us-east-1.amazonaws.com/440848399208/mvu2ab"
 sqs = boto3.client('sqs')
 
 def delete_message(handle):
@@ -19,40 +20,58 @@ def delete_message(handle):
         print(e.response['Error']['Message'])
 
 def get_message():
+    messages = []
     try:
-        # Receive message from SQS queue. Each message has two MessageAttributes: order and word
-        # You want to extract these two attributes to reassemble the message
-        response = sqs.receive_message(
-            QueueUrl=url,
-            AttributeNames=[
+        while True:
+            response = sqs.receive_message(
+                QueueUrl=url,
+                AttributeNames=[
                 'All'
             ],
-            MaxNumberOfMessages=1,
+            MaxNumberOfMessages=10,
             MessageAttributeNames=[
                 'All'
             ]
         )
         # Check if there is a message in the queue or not
         if "Messages" in response:
-            # extract the two message attributes you want to use as variables
-            # extract the handle for deletion later
-            order = response['Messages'][0]['MessageAttributes']['order']['StringValue']
-            word = response['Messages'][0]['MessageAttributes']['word']['StringValue']
-            handle = response['Messages'][0]['ReceiptHandle']
-
-            # Print the message attributes - this is what you want to work with to reassemble the message
-            print(f"Order: {order}")
-            print(f"Word: {word}")
-
-        # If there is no message in the queue, print a message and exit    
+            for message in response['Messages']:
+                order = response['Messages']['MessageAttributes']['order']['StringValue']
+                word = response['Messages']['MessageAttributes']['word']['StringValue']
+                handle = response['Messages']['ReceiptHandle']
+                messages.append({'order':order,'word': word, 'ReceiptHandle':handle})
         else:
-            print("No message in the queue")
-            exit(1)
-            
-    # Handle any errors that may occur connecting to SQS
+             print("No message in the queue")
+           
+        
     except ClientError as e:
         print(e.response['Error']['Message'])
+    return messages
 
-# Trigger the function
+def reassemble_phrase(messages):
+    sorted_messages = sorted(messages, key=lambda x: int(x['order']))
+    phrase = ' '.join(message['word'] for message in sorted_messages)
+    return phrase
+    
+def main():
+    messages = get_message()
+
+    if messages:
+        print("Received messages:", messages)
+        phrase = reassemble_phrase(messages)
+        print("Reassembled phrase:", phrase)
+
+        for message in messages:
+            delete_message(message['ReceiptHandle'])
+
+        with open('phrase.txt', 'w') as f:
+            f.write(phrase + '\n')
+        
+    else:
+        print("No messages found in queue.")
+
 if __name__ == "__main__":
-    get_message()
+    main()
+ 
+ 
+
